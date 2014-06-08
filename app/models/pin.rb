75a67522bc450724ac2c3d70fbb6d86cb987f8a9
Tobias Walter <tobi@team.shoeboxed.com>
2014-06-08 03:52:28 -0400

    Troubleshooting Heroku app doesn't let me create new pins - changed pin.rb file

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>"}
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>"} :storage => :s3, :bucket => 'Tobi-Pinteresting', :s3_credentials => {:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
 
